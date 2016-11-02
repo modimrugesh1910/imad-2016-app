a1e2b8a99d5f9fb63278b07a358f787ac93f7ecb
modimrugesh1910 <modimrugesh1910@gmail.com>
2016-11-02 14:52:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.php'));
+});
+
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
