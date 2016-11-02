4add8a351553de228cc7d5ce2f6d1b000982338c
modimrugesh1910 <modimrugesh1910@gmail.com>
2016-11-03 02:10:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,63 @@
 console.log('Loaded!');
 
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+    
+    // create a request object
+    var request = new XMLHttpRequest();
+    
+    // capture the responce and store it in a vsriable
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE) {
+            // take some action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    // make the request
+    request.open("GET","http://modimrugesh1910.imad.hasura-app.io/counter", true);
+    request.send(null);
+};
+
+/*
+// submit name
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    // make a request to the server and send the name
+    
+    // create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture a list of names and render it as a list
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            // take some action
+            if(request.status === 200) {
+                // capture a list of name and render it as a list  
+                var names = request.responceText;     //['name1', 'name2', 'name3', 'name4'];
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+        
+    };
+    // make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    
+    request.open('GET', 'http://modimrugesh1910.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
+};
+*/
 // login javascript
 
 $('.form').find('input, textarea').on('keyup blur focus', function (e) {
