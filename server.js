0d0b834d2267bfaf495d57915b370bced85d5d36
modimrugesh1910 <modimrugesh1910@gmail.com>
2016-11-03 02:10:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,72 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config = {
+    user: 'modimrugesh1910',
+    database: 'modimrugesh1910',
+    host: 'db.imad.hasura-app.io',
+    port: '8080',
+    password: process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
+
+var articles = { 
+    'article-one' : {
+        title: 'Article One ',
+        heading: 'Article One',
+        date: 'Oct 7, 2016',
+        content: `
+            <p>
+                This is my first article. This is my first article. This is my first article. This is my first article. This is my first article.This is my first article. This is my first article. This is my first article. This is my first article. 
+            </p>
+            <p>
+                This is my first article. This is my first article. This is my first article. This is my first article. This is my first article.This is my first article. This is my first article. This is my first article. This is my first article. 
+            </p>
+            <p>
+                This is my first article. This is my first article. This is my first article. This is my first article. This is my first article.This is my first article. This is my first article. This is my first article. This is my first article. 
+            </p>
+    }
+};
+
+function createTemplate(data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr />
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 
 app.get('/ui/index.php', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.php'));
@@ -17,10 +80,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter', function(req, res){
-    counter = counter + 1;
-    res.send(counter.toString());
+var pool = new Pool(config);
+app.get('/', function (req, res) {
+    // make a select request
+    // return a responce with the results
+    
+    pool.query('SELECT * FROM test', function(err, result) {
+       if(err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.Stringify(result, rows));
+       }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -33,6 +104,28 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.scss'));
+});
+
+var counter = 0;
+app.get('/counter', function(req, res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
+app.get('/:articleName', function(req, res) {
+    // articleName === article-one
+    // articles[articleName] === {} content object for article one
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
+
+var names = [];
+app.get('/submit-name', function(req, res) { // URL: /submit-name?name-xxxxxx
+    // get the name from the request
+    var name = req.params.name;
+    names.push(name);
+    // JSON - JavaScript Object Notation
+    res.send(JSON.stringify(names));
 });
 
 app.get('/ui/madi.png', function (req, res) {
